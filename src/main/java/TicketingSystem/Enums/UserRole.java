76784872502f035664369a287573639b727e512b
afrahimran <147979915+afrahimran@users.noreply.github.com>
2025-03-25 11:58:11 +0530

    Delete src directory

--- src/main/java/TicketingSystem/Enums/UserRole.java
+++
@@ -1,7 +0,0 @@
-// enum representing different user roles in the system
-package TicketingSystem.Enums;
-
-public enum UserRole {
-    CLIENT, // regular client user
-    COMPANY // business or company user
-}
